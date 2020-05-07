a2403a5f86ca8ec8ce50575cb03d71dd813ebb47
shiv <shivssharma1990@gmail.com>
2020-05-07 20:14:51 +0530

    added portfolio basic structure

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from portfolio.views import home
 
 urlpatterns = [
+    path('', home, name="home"),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
